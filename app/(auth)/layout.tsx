c98147078fac4a3d5ca3f527b071c48e3fc78615
NikaEliashvili <nikaeliashvili18@outlook.com>
2024-08-28 14:33:39 +0400

    test

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex justify-center items-center h-full ">
+      {children}
+    </div>
+  );
+}
---
+++ app/(root)/[[...rest]]/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { useEffect } from "react";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+const SetupPage = () => {
+  const { onOpen, isOpen } = useStoreModal();
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div className="p-4">Root Page</div>;
+};
+
+export default SetupPage;
